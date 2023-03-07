198c4936560bdd9b39267f53e8f6865140ad037d
Kelvin Etori <kelvin.8.kast@gmail.com>
2023-03-07 05:41:27 -0700

    made scrollable, updated Preview

--- src/componets/Preview.tsx
+++ src/componets/Preview.tsx
@@ -14,9 +14,7 @@ const Preview = (props: Props) => {
         <div className="project">
           <p className="project-name">{project.title}</p>
           <span>|</span>
-          <a href="#" className="project-link">
-            {project.link}
-          </a>
+          <button className="project-link">{project.link}</button>
         </div>
         <div className="project-list">
           <ul>
@@ -31,12 +29,64 @@ const Preview = (props: Props) => {
     );
   });
 
-  const projectCheck = () => {
-    if (projects.length !== 0) {
+  const personalCheck = () => {
+    const site = text.website !== "" ? <button>Personal site: {text.website}</button> : null;
+    const github = text.github !== "" ? <button>Github: {text.github}</button> : null;
+    const linkedin = text.linkedin !== "" ? <button>Linkdin: {text.linkedin}</button> : null;
+    const links = [site, github, linkedin];
+    if (
+      text.name.length !== 0 ||
+      text.phone.length !== 0 ||
+      text.email.length !== 0 ||
+      text.website.length !== 0 ||
+      text.linkedin.length !== 0 ||
+      text.github.length !== 0
+    ) {
       return (
-        <div className="project-info">
-          <h1>Projects</h1>
-          {projects}
+        <div className="personal-info">
+          <div className="personal-contact">
+            <div className="name">
+              <p>{text.name}</p>
+            </div>
+            <div className="contact">
+              <a href="mailto:kelvinkamau1130@yahoo.com">{text.email}</a>
+              <span>|</span>
+              <p>{text.phone}</p>
+            </div>
+          </div>
+          <div className="personal-links">{links}</div>
+        </div>
+      );
+    }
+  };
+
+  const educationCheck = () => {
+    if (text.college.length !== 0 || text.major.length !== 0 || text.graduation.length !== 0 || text.gpa.length !== 0) {
+      return (
+        <div className="education-info">
+          <h1>Education</h1>
+          <div className="education">
+            <div className="univeristy-info">
+              <p id="university-name">{text.college}</p>
+              <p id="univeristy-dates">{text.graduation}</p>
+            </div>
+            <div className="major-info">
+              <p id="major-degree">{text.major}</p>
+              <span>|</span>
+              <p id="GPA">GPA: {text.gpa}</p>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  const experienceCheck = () => {
+    if (text.experience.length !== 0) {
+      return (
+        <div className="professional-info">
+          <h1>Professional Experience</h1>
+          {experiences}
         </div>
       );
     }
@@ -66,12 +116,12 @@ const Preview = (props: Props) => {
     );
   });
 
-  const experienceCheck = () => {
-    if (text.experience.length !== 0) {
+  const projectCheck = () => {
+    if (projects.length !== 0) {
       return (
-        <div className="professional-info">
-          <h1>Professional Experience</h1>
-          {experiences}
+        <div className="project-info">
+          <h1>Projects</h1>
+          {projects}
         </div>
       );
     }
@@ -82,66 +132,16 @@ const Preview = (props: Props) => {
     const frameworks =
       text.frameworks.length !== 0 ? <p className="tech-frame">Frameworks: {text.frameworks}</p> : null;
     const tools = text.tools.length !== 0 ? <p className="tech-tools">Tools: {text.tools}</p> : null;
+    const skills = [languages, frameworks, tools];
+    if (languages === null && frameworks === null && tools === null) {
+      return null;
+    }
     return (
       <div className="tech-skills">
         <h1>Skills</h1>
-        {languages}
-        {frameworks}
-        {tools}
+        {skills}
       </div>
     );
-  };
-
-  const educationCheck = () => {
-    if (text.college.length !== 0 || text.major.length !== 0 || text.graduation.length !== 0 || text.gpa.length !== 0) {
-      return (
-        <div className="education-info">
-          <h1>Education</h1>
-          <div className="education">
-            <div className="univeristy-info">
-              <p id="university-name">{text.college}</p>
-              <p id="univeristy-dates">{text.graduation}</p>
-            </div>
-            <div className="major-info">
-              <p id="major-degree">{text.major}</p>
-              <span>|</span>
-              <p id="GPA">GPA: {text.gpa}</p>
-            </div>
-          </div>
-        </div>
-      );
-    }
-  };
-
-  const personalCheck = () => {
-    if (
-      text.name.length !== 0 ||
-      text.phone.length !== 0 ||
-      text.email.length !== 0 ||
-      text.website.length !== 0 ||
-      text.linkedin.length !== 0 ||
-      text.github.length !== 0
-    ) {
-      return (
-        <div className="personal-info">
-          <div className="personal-contact">
-            <div className="name">
-              <p>{text.name}</p>
-            </div>
-            <div className="contact">
-              <a href="mailto:kelvinkamau1130@yahoo.com">{text.email}</a>
-              <span>|</span>
-              <p>{text.phone}</p>
-            </div>
-          </div>
-          <div className="personal-links">
-            <a href="#">Personal site: {text.website}</a>
-            <a href="#">Linkdin: {text.linkedin}</a>
-            <a href="#">Github: {text.github}</a>
-          </div>
-        </div>
-      );
-    }
   };
 
   return (
